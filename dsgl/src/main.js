6f53451794405d28fc7056f9b909d474d55a4e6a
Your Name <you@example.com>
2021-03-09 09:57:45 +0800

    a

--- dsgl/src/main.js
+++ dsgl/src/main.js
@@ -9,6 +9,38 @@ import VueAxios from "vue-axios";
 Vue.use(VueAxios, axios);
 Vue.config.productionTip = false;
 
+// 添加响应拦截器
+axios.interceptors.response.use(
+  function(response) {
+    // 对响应数据做点什么
+    if (response.data.status === 400) {
+      router.replace("/");
+    }
+    return response.data;
+  },
+  function(error) {
+    // 对响应错误做点什么
+    return Promise.reject(error);
+  }
+);
+
+// 添加请求拦截器
+axios.interceptors.request.use(
+  function(config) {
+    // 在发送请求之前做些什么
+    if (config.url === '/login' || config.url === '/register') {
+      return config;
+  }else{
+      let token=sessionStorage.getItem('token');
+      config.headers['token'] = token;
+  }
+    return config;
+  },
+  function(error) {
+    // 对请求错误做些什么
+    return Promise.reject(error);
+  }
+);
 new Vue({
   router,
   render: (h) => h(App),
